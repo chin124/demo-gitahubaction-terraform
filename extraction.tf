d80e73125e402c0a8dbb6f6cbe3128e588802378
unknown <cdesai2712@gmail.com>
2024-02-26 16:54:11 +0530

    github actions pipeline

--- extraction.tf
+++ extraction.tf
@@ -1,10 +1,10 @@
 # provider
-provider "aws" {
-    region = "us-east-1"
+#provider "aws" {
+    #region = "us-east-1"
     #access_key = "ASIAR3AX26EEQVCGCAWH"
     #secret_key = "AE36ejH92yw5RoSVc0RAHZ7UHfl8aZC0Zp5ygj9B"
     #token = "FwoGZXIvYXdzEN7//////////wEaDGjsSuMpLhzJEMb1xSLJAfAgStYjYT49VzbNeRgmqszOFG3dObEh3okj4AED9v7YPo9xk1JulLSNIc/OsNc73kmEiWrs9DIyKI25CRhVbjt3a0kddGev7UXAJNsX4MlydUD/9QxhmZSytZESzrtn+k8gX3TuCfUdeJNTW0nLqOQOkKyQD3VjkqEO0jNbL3c8A/im5YYPjcvjj00pztk9v3IOYIpqKwBGYyoM0eTwBgKW8kmwVoVSZ7Nft/vuUdBfkf6pZcyCkPAApe2TjTLO8QGQURo1562LDSiPovCuBjIt3omTt/p8SkTbIoXUU8o+3R8CMWjg9vYKa4AML0HRqhP2Q6oRv7JNBxXzsWMx"
-  }
+  #}
 # generate randomised names
 resource "random_id" "random_id_generator" {
     byte_length = 8
