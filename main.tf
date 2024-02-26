42b76f1193e1f7e56c8cb26450953c56542bced0
unknown <cdesai2712@gmail.com>
2024-02-26 17:59:19 +0530

    github actions pipeline

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-terraform{
-      cloud {
+terraform {
+  cloud {
     organization = "group4dbda123"
 
     workspaces {
-      name = "group4dbda123"
+      name = "Terraformbigdata"
     }
   }
-} 
-
+}
